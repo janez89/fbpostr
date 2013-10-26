d47f0072a7ee5b0b11ba5e69ad502832b4330e82
janez <mjanee@gmail.com>
2013-10-26 17:17:39 +0200

    added post status filter

--- public/app/controllers/PostsCtrl.js
+++ public/app/controllers/PostsCtrl.js
@@ -1,29 +1,27 @@
 
-postrApp.controller('PostsCtrl', function PostsCtrl ($scope, $resource, $http, ngTableParams) {
+postrApp.controller('PostsCtrl', function PostsCtrl ($scope, $resource, $http, $routeParams, ngTableParams) {
 	$scope.loading = true;
+    $scope.type = $routeParams.type;
+
 	var Post = $resource('/post/:id', {'id': '@id'});
 
     $scope.tableParams = new ngTableParams({
         page: 1, // show first page
         total: 0, // length of data
         count: 10, // count per page
-        counts: [5, 10, 20, 50],
-        sorting: {
-            created: 'desc'
-        }
+        counts: []
     });
 
     var getData = function () {
         $scope.loading = true;
-        // ajax request to api
-        /*Log.get($scope.tableParams.url(), function (data) {
-            $scope.logs = data.docs;
-            $scope.tableParams.total = data.count;
-            $scope.loading = false;
-        });*/
-        $http.get('/post').then(function (resp) {
+        var qs = '';
+        if ($routeParams.type)
+            qs = '?status='+ ($routeParams.type === 'waiting' ? 0 : 1 );
+
+        $http.get('/post'+ qs).then(function (resp) {
             $scope.posts = resp.data.docs;
             $scope.tableParams.total = resp.data.count;
+            $scope.loading = false;
         });
     };
 
