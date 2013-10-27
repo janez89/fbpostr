fd0f41c3dd42c1b59ab0bc90db02f189800ccb12
janez <mjanee@gmail.com>
2013-10-27 19:34:37 +0100

    added authentication methods

--- public/app/app.js
+++ public/app/app.js
@@ -4,53 +4,97 @@ var postrApp = angular.module('postrApp', [
 	'ngResource', 'ngCookies', 'ngTable'
 ]);
 
-postrApp.config(['$routeProvider', function ($routeProvider) {
+postrApp.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
 	$routeProvider.
 	when('/', {
 		templateUrl: '/app/views/home.html',
-		controller: 'HomeCtrl'
+		controller: 'HomeCtrl',
+		restrict: true
 	}).
 	when('/404', {
 		templateUrl: '/app/views/404.html',
-		controller: 'HomeCtrl'
+		controller: 'HomeCtrl',
+		restrict: false
 	}).
 	when('/login', {
 		templateUrl: '/app/views/login.html',
-		controller: 'LoginCtrl'
+		controller: 'LoginCtrl',
+		restrict: false
 	}).
 	when('/posts/:type', {
 		templateUrl: '/app/views/posts.html',
-		controller: 'PostsCtrl'
+		controller: 'PostsCtrl',
+		restrict: true
 	}).
 	when('/posts', {
 		templateUrl: '/app/views/posts.html',
-		controller: 'PostsCtrl'
+		controller: 'PostsCtrl',
+		restrict: true
 	}).
 	when('/post/:id', {
 		templateUrl: '/app/views/view-post.html',
-		controller: 'ViewPostCtrl'
+		controller: 'ViewPostCtrl',
+		restrict: true
 	}).
 	when('/newpost/:type/:uid', {
 		templateUrl: '/app/views/new-post.html',
-		controller: 'NewPostCtrl'
+		controller: 'NewPostCtrl',
+		restrict: true
 	}).
 	when('/newpost/:type', {
 		templateUrl: '/app/views/new-post.html',
-		controller: 'NewPostCtrl'
+		controller: 'NewPostCtrl',
+		restrict: true
 	}).
 	when('/newpost', {
 		templateUrl: '/app/views/new-post.html',
-		controller: 'NewPostCtrl'
+		controller: 'NewPostCtrl',
+		restrict: true
 	}).
 	when('/repost/:id', {
 		templateUrl: '/app/views/re-post.html',
-		controller: 'RePostCtrl'
+		controller: 'RePostCtrl',
+		restrict: true
 	}).
 	when('/settings', {
 		templateUrl: '/app/views/settings.html',
-		controller: 'SettingsCtrl'
+		controller: 'SettingsCtrl',
+		restrict: true
 	}).
 	otherwise({
 		redirectTo: '/404'
 	});
+
+	// http interceptor for session timeout
+	var interceptor = ['$location', '$q', function($location, $q) {
+		function success(response) {
+			return response;
+		}
+
+		function error(response) {
+			if (response.status === 401) {
+				$location.path('/login');
+				return $q.reject(response);
+			}
+			else {
+				return $q.reject(response);
+			}
+		}
+
+		return function(promise) {
+			return promise.then(success, error);
+		};
+	}];
+
+	$httpProvider.responseInterceptors.push(interceptor);
+}]);
+
+postrApp.run(['$rootScope', '$location', 'Auth', function ($rootScope, $location, Auth) {
+
+    $rootScope.$on("$routeChangeStart", function (event, next, current) {
+        if (next.restrict && !Auth.isLoggedIn()) {
+            $location.path('/login');
+        }
+    });
+
 }]);
