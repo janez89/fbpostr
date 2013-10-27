d76d192a99f12c3448fda472101d63dceac8783f
janez <mjanee@gmail.com>
2013-10-27 20:20:01 +0100

    fixed fn call

--- public/app/services.js
+++ public/app/services.js
@@ -2,29 +2,27 @@
 postrApp.factory('Auth', function ($http, $rootScope, $cookieStore) {
 	'use strict';
 	
-	var isLoggedIn = $cookieStore.get('login') || false;
+	var isLoggedIn = $cookieStore.get('login') || true;
 
 	var changeState = function (state) {
 		isLoggedIn = state;
 		$cookieStore.put(state);
 	};
 	return {
-
 		isLoggedIn: function() {
-			return isLoggedIn === true;
+			return isLoggedIn == true;
 		},
-
 		login: function(user, success, error) {
 			$http.post('/login', user).success(function (data) {
 				changeState( true );
 				success();
 			}).error(error || function () {});
 		},
-
 		logout: function(success, error) {
 			$http.get('/logout').success(function () {
 				changeState( false );
-				success();
+				if (success)
+					success();
 			}).error(error || function () {});
 		}
 	};
