57c4de45449232d9c0ea1ab8642c835c726c1ff2
janez <mjanee@gmail.com>
2013-10-27 20:20:22 +0100

    logout

--- public/app/controllers/LoginCtrl.js
+++ public/app/controllers/LoginCtrl.js
@@ -2,6 +2,10 @@
 postrApp.controller('LoginCtrl', function LoginCtrl ($scope, Auth, $location) {
 	$scope.loginError = false;
 
+	if (Auth.isLoggedIn()) {
+		Auth.logout();
+	}
+
 	$scope.doLogin = function () {
 		Auth.login($scope.user, function () {
 			$location.path('/');
