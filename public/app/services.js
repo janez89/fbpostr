49ebf012750ba64dddb219c353bc5d25c25ca58f
janez <mjanee@gmail.com>
2013-10-27 19:34:05 +0100

    Auth service

---
+++ public/app/services.js
@@ -0,0 +1,32 @@
+
+postrApp.factory('Auth', function ($http, $rootScope, $cookieStore) {
+	'use strict';
+	
+	var isLoggedIn = $cookieStore.get('login') || false;
+
+	var changeState = function (state) {
+		isLoggedIn = state;
+		$cookieStore.put(state);
+	};
+	return {
+
+		isLoggedIn: function() {
+			return isLoggedIn === true;
+		},
+
+		login: function(user, success, error) {
+			$http.post('/login', user).success(function (data) {
+				changeState( true );
+				success();
+			}).error(error || function () {});
+		},
+
+		logout: function(success, error) {
+			$http.get('/logout').success(function () {
+				changeState( false );
+				success();
+			}).error(error || function () {});
+		}
+	};
+
+});
